4728baaf7185fcaf5301d17cdbffb8979604fc9f
Your NameChuMinhLyGit <ly.chuminh@gmail.com>
2020-09-13 23:43:55 +0700

    L3 Giam Array 1 va Tang Array 2

--- app/src/main/java/com/example/gamloto/MainActivity.java
+++ app/src/main/java/com/example/gamloto/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gamloto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,73 +10,123 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mEdtSoMin, mEdtSoMax;
-    Button mBtnRandom;
+    Button mBtnRandom,mBtnReset,mbtnAddRang;
     TextView mTvResult;
     String mKetQua="";
-    int _Even = 0;
-    int _arrEven[] = { };
+
+    List<Integer> arrNum = new ArrayList<>();
+    int _SoMin = 0;
+    int _SoMax = 0;
+    int mIndex = -1;
+    int Result =-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mEdtSoMin = findViewById(R.id.edtSoMin);
         mEdtSoMax = findViewById(R.id.edtSoMax);
         mBtnRandom = findViewById(R.id.btnRandom);
-        mTvResult = findViewById(R.id.tvResult);
+        //mTvResult = findViewById(R.id.tvResult);
+        mTvResult= (TextView) findViewById(R.id.tvResult);
+        mTvResult.setMovementMethod(new ScrollingMovementMethod());
+
+        mbtnAddRang = findViewById(R.id.btnAddrang);
+        mBtnReset = findViewById(R.id.btnReset);
+        disableView(mBtnRandom);
+
+        mbtnAddRang.setOnClickListener(new View.OnClickListener()
+           {
+               @Override
+               public void onClick(View view) {
+                   String tSoMin = mEdtSoMin.getText().toString();
+                   String tSoMax = mEdtSoMax.getText().toString();
+
+                   if (tSoMin.isEmpty()||tSoMax.isEmpty())
+                   {
+                       //Log.d("BBB2","Thong Bao :" + tSoMin +" , "+tSoMax);
+                       Toast.makeText(MainActivity.this,"Ban Chua Nhap Du Thong Tin", Toast.LENGTH_LONG).show();
+                       return; // nếu không đúng thì ngưng các câu lệnh phía sau.
+                   }
+
+                   // Chuyển dữ liệu thành số
+                    _SoMin = Integer.parseInt(tSoMin);
+                    _SoMax = Integer.parseInt(tSoMax);
+
+                   if (_SoMin>_SoMax)
+                   {
+                       _SoMax = _SoMin+1;
+                   }
+                   mEdtSoMin.setText(String.valueOf(_SoMin));
+                   mEdtSoMax.setText(String.valueOf(_SoMax));
+                   arrNum.clear();
+                   for (int i = _SoMin; i <=_SoMax ; i++)
+                   {
+                        arrNum.add(i);
+                   }
+                   disableView(mbtnAddRang);
+                   disableView(mEdtSoMax);
+                   disableView(mEdtSoMin);
+                   enableView(mBtnRandom);
+               }
+           }
+        );
+        // thực hiện nút reset
+        // mbtnRandom dissable
+        // mtbnAddrange Enable
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTvResult.setText("");
+                disableView(mBtnRandom);
+                enableView(mbtnAddRang);
+                enableView(mEdtSoMax);
+                enableView(mEdtSoMin);
+            }
+        });
 
         mBtnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                String tSoMin = mEdtSoMin.getText().toString();
-                String tSoMax = mEdtSoMax.getText().toString();
-
-                if (tSoMin.isEmpty()||tSoMax.isEmpty())
+                if (arrNum.size()>0)
                 {
-                    //Log.d("BBB2","Thong Bao :" + tSoMin +" , "+tSoMax);
-                    Toast.makeText(MainActivity.this,"Ban Chua Nhap Du Thong Tin", Toast.LENGTH_LONG).show();
-                    return; // nếu không đúng thì ngưng các câu lệnh phía sau.
+                //    mTvResult.setText(arrNum.toString());
+                    Random random = new Random();
+                    mIndex = random.nextInt(arrNum.size());
+                    Result = arrNum.get(mIndex);
+                    // Sử dụng mảng hiện tại còn những số nào chưa được lấy ra và Số lấy ra chạy từng bước
+                    mKetQua += arrNum.toString()+":"+ String.valueOf(Result) + "\n";
+                    // Sử dụng nguyên thủy theo từng giá trị trong mảng và có dấu - từng số.
+                   // mKetQua += arrNum.size()!=1? Result + "-":Result;
+                    // sử dụng phương thức so sánh nếu chỉ còn 1 phần tử thì bỏ đi dấu - ở cuối
+                    //arrNum.toString()+":"+ String.valueOf(Result) + "\n";
+                    mTvResult.setText(mKetQua);
+                    arrNum.remove(mIndex);
+                }
+                else
+                    {
+                    Toast.makeText(MainActivity.this,"Hết Giá trị Random",Toast.LENGTH_SHORT).show();
                 }
 
-                // Chuyển dữ liệu thành số
-                int _SoMin = Integer.parseInt(tSoMin);
-                int _SoMax = Integer.parseInt(tSoMax);
-
-                if (_SoMin>_SoMax)
-                {
-                    _SoMax = _SoMin+1;
-                }
-                mEdtSoMin.setText(String.valueOf(_SoMin));
-                mEdtSoMax.setText(String.valueOf(_SoMax));
-
-                Random random = new Random();
-                int Value = random.nextInt(_SoMax-_SoMin+1)+_SoMin;
-
-                if (_Even > 0)
-                {
-                    for (int j=0; j <_Even ; j++) {
-                      if (_arrEven[j] == Value)
-                        {
-                            Toast.makeText(MainActivity.this, "Trung Du Lieu :" + Value, Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                    }
-                }
-                _arrEven = addX(_Even, _arrEven, Value);
-                _Even++;
-                //mKetQua += Value + "-";
-           //     mKetQua = Arrays.toString(_arrEven);
-                mKetQua += Value + "-";
-                mTvResult.setText(mKetQua);
             }
         });
+    }
+    //
+    private void disableView(View v)
+    {
+        v.setEnabled(false);
+    }
+    private void enableView(View v)
+    {
+        v.setEnabled(true);
     }
 
     public static int[] addX(int n, int arr[], int x)
@@ -84,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
         int newarr[] = new int[n + 1];
         for (i = 0; i < n; i++)
             newarr[i] = arr[i];
-        newarr[n] = x;
+            newarr[n] = x;
         return newarr;
     }
 }
